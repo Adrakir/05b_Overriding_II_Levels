de5929a3eb55d8bf6be8bb7e3f468ac65bdc494c
Adrakir <168722517+Adrakir@users.noreply.github.com>
2024-05-28 10:16:10 +0200

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,26 +1,46 @@
 package com.cc.java;
 
-public class App {
+public class PacMan {
+    private static int pacX = 1; // Pac-Mans Startposition
+    private static int pacY = 1;
+    private static int score = 0;
+
     public static void main(String[] args) {
-    
-        Player player = new Player();
+        char[][] maze = { 
+            {'#', '#', '#', '#', '#'},
+            {'#', '.', '.', '.', '#'},
+            {'#', '.', '#', '.', '#'},
+            {'#', '.', '.', '.', '#'},
+            {'#', '#', '#', '#', '#'}
+        };
 
-        
-        // Variante 1
-        // for (int i = 0; i < 12; i++) {
-        //     output(player.play());  
-        // }
+        Scanner scanner = new Scanner(System.in);
 
-        // Variante 2
-        // while (player.points < 13) {
-        //     output(player.play()); 
-        // }
+        while (true) {
+            printMaze(maze);
+            System.out.println("Score: " + score);
 
+            System.out.print("Bewegung (w, a, s, d): ");
+            String move = scanner.nextLine();
 
-       
+            if (move.equals("w") && maze[pacY - 1][pacX] != '#') pacY--;
+            if (move.equals("s") && maze[pacY + 1][pacX] != '#') pacY++;
+            if (move.equals("a") && maze[pacY][pacX - 1] != '#') pacX--;
+            if (move.equals("d") && maze[pacY][pacX + 1] != '#') pacX++;
+
+            if (maze[pacY][pacX] == '.') {
+                score++;
+                maze[pacY][pacX] = ' ';
+            }
+        }
     }
 
-    private static void output(String outStr) {
-        System.out.println(outStr);
+    private static void printMaze(char[][] maze) {
+        for (char[] row : maze) {
+            for (char cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
     }
 }
